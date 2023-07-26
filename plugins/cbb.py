9f8305badb5db3fe932d4f2a725e454f99fa32c6
Arisu0000 <130484247+Arisu0000@users.noreply.github.com>
2023-07-26 17:39:57 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={6693334935}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='DevLOVEper'>click here</a>\n○ Channel : @PanindiaFilmz\n○ Support Group : @BoTzUpdates0</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={6693334935}'>@PIFOficial_Dev</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='DevLOVEper'>click here</a>\n○ Channel : @PanindiaFilmz\n○ Support Group : @BoTzUpdates0</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
